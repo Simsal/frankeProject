fac2d752b4d4898f851894f19d0bd427b67c45b3
Simsal <simon_brueckl9@hotmail.com>
2015-09-28 07:55:18 +0200

    First commit

---
+++ src/main/java/de/franke/MarriageApplication.java
@@ -0,0 +1,12 @@
+package de.franke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarriageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarriageApplication.class, args);
+    }
+}
