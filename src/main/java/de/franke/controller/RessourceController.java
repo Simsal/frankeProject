8ada18b558ae4411b3ecf062db6dd0e2a4d2f14d
Simsal <simon_brueckl9@hotmail.com>
2015-10-05 09:36:17 +0200

    modularer Aufbau

--- src/main/java/de/franke/controller/RessourceController.java
+++ src/main/java/de/franke/controller/RessourceController.java
@@ -1,5 +1,6 @@
 package de.franke.controller;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -7,17 +8,20 @@ import java.util.UUID;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class RessourceController {
-
-	@RequestMapping("/resource")
-	  public Map<String,Object> home() {
-	    Map<String,Object> model = new HashMap<String,Object>();
-	    model.put("id", UUID.randomUUID().toString());
-	    model.put("content", "Hello World");
-	    return model;
-	  }
-	
-	  
-
-}
+//@RestController
+//public class RessourceController {
+//
+//	@RequestMapping("/resource")
+//	  public Map<String,Object> home() {
+//	    Map<String,Object> model = new HashMap<String,Object>();
+//	    model.put("id", UUID.randomUUID().toString());
+//	    model.put("content", "Hello World");
+//	    return model;
+//	  }
+//	@RequestMapping("/user")
+//	  public Principal user(Principal user) {
+//	    return user;
+//	  }
+//	  
+//
+//}
--- src/main/resources/static/js/marriage.js
+++ src/main/resources/static/js/marriage.js
@@ -1,6 +1,25 @@
-angular.module('marriage', [])
-.controller('home', function($scope, $http) {
-	  $http.get('/resource/').success(function(data) {
-	    $scope.greeting = data;
-	  })
-	});
+angular
+		.module('hello', [ 'ngRoute', 'auth', 'home', 'navigation' ])
+		.config(
+
+				function($routeProvider, $httpProvider, $locationProvider) {
+
+					$locationProvider.html5Mode(true);
+
+					$routeProvider.when('/', {
+						templateUrl : 'js/home/home.html',
+						controller : 'home'
+					}).when('/login', {
+						templateUrl : 'js/navigation/login.html',
+						controller : 'navigation'
+					}).otherwise('/');
+
+					$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+				}).run(function(auth) {
+
+			// Initialize auth module with the home page and login/logout path
+			// respectively
+			auth.init('/', '/login', '/logout');
+
+		});
