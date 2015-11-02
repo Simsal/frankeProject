556254996bfdc6fc73407371e2c9259295acc21a
Simsal <simon_brueckl9@hotmail.com>
2015-11-02 15:13:35 +0100

    Gästeliste

--- target/classes/static/js/guestList/guestList.js
+++ target/classes/static/js/guestList/guestList.js
@@ -1,5 +1,24 @@
 angular.module('guestList', []).controller('guestList', function($scope) {
-	$scope.guests = [{ name : 'simon',
-						postalCode : '78954'}]
-	$scope.isDisabled = true;
+	$scope.guests = [{ firstName : 'simon',
+						postalCode : '78954',
+						isDisabled : true},
+					{firstName : 'sarah',
+						postalCode : '78953',
+						isDisabled : true}]
+	$scope.newGuest = {};
+	
+	$scope.delete = function (index, item) {
+//		modalService.showModal({}).then(function (result){
+//		var string = item._links.self.href;
+//		$scope.helpArray = string.split('/');
+//		var helpstring = $scope.helpArray[$scope.helpArray.length-1]
+//		$scope.array = helpstring.split('{')
+//        $http.delete('delete/' + $scope.array[0]).success(function(data, status){
+//        })
+//		
+//		
+//		})
+		$scope.guests.splice(index, 1);
+    }
+	
 });
